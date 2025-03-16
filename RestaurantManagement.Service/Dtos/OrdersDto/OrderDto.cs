ff06b3c4c4a730f71817c0a4ed6efe302428b232
Long Ma <maduylong04@gmail.com>
2025-03-17 00:48:12 +0700

    Update OrderService

--- RestaurantManagement.Service/Dtos/OrdersDto/OrderDto.cs
+++ RestaurantManagement.Service/Dtos/OrdersDto/OrderDto.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace RestaurantManagement.Service.Dtos.OrderDto
+namespace RestaurantManagement.Service.Dtos.OrdersDto
 {
     public class OrderDTO
     {
--- RestaurantManagement.Service/Implementation/OrderService.cs
+++ RestaurantManagement.Service/Implementation/OrderService.cs
@@ -1,57 +1,37 @@
-﻿using RestaurantManagement.DataAccess.Interfaces;
+﻿using AutoMapper;
+using RestaurantManagement.Core.ApiModels;
+using RestaurantManagement.DataAccess.Interfaces;
 using RestaurantManagement.DataAccess.Models;
-using RestaurantManagement.Service.Dtos.OrderDto;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using RestaurantManagement.Service.Dtos.OrdersDto;
+using RestaurantManagement.Service.Interfaces;
 
 namespace RestaurantManagement.Service.Implementation
 {
-    public class OrderService
+    public class OrderService : BaseService, IOrderService
     {
+        private readonly IMapper _mapper;
+        private readonly IRepository<TblOrderInfo> _orderInfoRepository;
+        private readonly IRepository<TblOrderDetail> _orderDetailsRepository;
         private readonly IOrderRepository _orderRepository;
 
-        public OrderService(IOrderRepository orderRepository)
+        public OrderService(
+            AppSettings appSettings,
+            IMapper mapper,
+            IRepository<TblOrderInfo> orderInfoRepository,
+            IRepository<TblOrderDetail> orderDetailsRepository,
+            IOrderRepository orderRepository
+        ) : base(appSettings, mapper) // Truyền xuống BaseService
         {
+            _orderInfoRepository = orderInfoRepository;
+            _orderDetailsRepository = orderDetailsRepository;
             _orderRepository = orderRepository;
+            _mapper = mapper;
         }
 
-        public async Task<OrderDTO> CreateOrderAsync(OrderDTO orderDto)
+        public async Task<OrderDTO> GetOrderByIdAsync(Guid orderId)
         {
-            var order = new TblOrderInfo
-            {
-                OrdId = Guid.NewGuid(),
-                CusId = orderDto.CusID,
-                TbiId = orderDto.TblID,
-                TotalPrice = orderDto.OrderDetails.Sum(d => d.Price * d.OdtQuantity),
-                CreatedAt = DateTime.UtcNow,
-            };
-
-            var orderDetails = orderDto.OrderDetails.Select(d => new TblOrderDetail
-            {
-                OdtId = Guid.NewGuid(),
-                MnuId = d.MnuID,
-                OdtQuantity = d.OdtQuantity,
-                CreatedAt = DateTime.UtcNow
-            }).ToList();
-
-            var createdOrder = await _orderRepository.CreateOrderAsync(order, orderDetails);
-
-            return new OrderDTO
-            {
-                OrdID = createdOrder.OrdId,
-                CusID = createdOrder.CusId,
-                TblID = createdOrder.TbiId,
-                TotalPrice = createdOrder.TotalPrice,
-                OrderDetails = orderDetails.Select(d => new OrderDetailDTO
-                {
-                    OdtID = d.OdtId,
-                    MnuID = d.MnuId,
-                    OdtQuantity = d.OdtQuantity
-                }).ToList()
-            };
+            var order = await _orderRepository.GetOrderByIdAsync(orderId);
+            return _mapper.Map<OrderDTO>(order);
         }
     }
 }
