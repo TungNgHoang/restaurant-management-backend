56c89c7de1a3b482dbe489d7be87ccf95294b53c
Long Ma <maduylong04@gmail.com>
2025-08-19 18:18:01 +0700

    confirm

--- RestaurantManagement.Service/Dtos/ReserDto/UpdateReservationRequestDto.cs
+++ RestaurantManagement.Service/Dtos/ReserDto/UpdateReservationRequestDto.cs
@@ -2,13 +2,7 @@
 {
     public class UpdateReservationRequestDto
     {
-        public string TempCustomerName { get; set; }
-        public string TempCustomerPhone { get; set; }
-        public string TempCustomerEmail { get; set; }
         public Guid TbiId { get; set; }
-        public DateTime ResDate { get; set; } // Ngày và giờ đến
-        public DateTime ResEndTime { get; set; } // Khoảng thời gian ở lại
         public int ResNumber { get; set; }
-        public string Note { get; set; }
     }
 }
